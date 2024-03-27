33f0147b87951e67f687eabe9b368e2b666c3001
Semporchothi S <semporchothi@gmail.com>
2024-03-27 21:44:00 +0530

    fix: user creation end point changes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const User = require('../actions/user_actions')
 
 router.get('/', (req,  res)=> {res.json({success: true})})
-router.post('/user/add', User.create);
+router.post('/user/create', User.create);
 router.post('/user/login', User.login);
 
 router.post('/user/get_details', User.details);
